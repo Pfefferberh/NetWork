3c476658ab427aa8322640380ce74fe2a6b94001
unknown <pfefferberhorion@gmail.com>
2020-10-31 21:07:28 +0200

    fineLY i have got it

---
+++ Skreeen/Skreeen/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skreeen
+{
+    class Program
+    {
+        static IPAddress pAddress = IPAddress.Parse("127.0.0.1");
+        static int port = 8888;
+        static TcpListener server = new TcpListener(pAddress, port);
+        static TcpClient client = null;
+        static NetworkStream networkStream = null;
+        static void Main(string[] args)
+        {
+            server.Start();
+            try
+            {
+
+                while (true)
+                {
+                    Console.WriteLine("Ожидание подключений... ");
+                    client = server.AcceptTcpClient();
+
+                    Console.WriteLine("Подключен клиент. Выполнение запроса...");
+
+                    networkStream = client.GetStream();
+
+                    string response = "Hello";
+                    byte[] data = Encoding.UTF8.GetBytes(response);
+
+                    networkStream.Write(data, 0, data.Length);
+
+                    int bytes = networkStream.Read(data, 0, data.Length);
+                    string temp = Encoding.UTF8.GetString(data, 0, bytes);
+                    Console.WriteLine(temp);
+
+                    Bitmap screen = new Bitmap(1000, 1000);
+                    using (Graphics g = Graphics.FromImage(screen))
+                    {
+                        g.CopyFromScreen(5, 5, 00, 0, screen.Size);
+                        screen.Save("test.Png", ImageFormat.Png);
+                    }
+                    Image image = Image.FromFile("test.Png");
+                    byte[] xByte = File.ReadAllBytes("test.Png"); 
+
+                    byte[] lng = BitConverter.GetBytes(xByte.Length);
+                    networkStream.Write(lng, 0, lng.Length);
+                    int umova = xByte.Length / 8;
+                    for (int i = 0; i < umova; i++)
+                    {
+                        if (i == 0)
+                            networkStream.Write(xByte, i, 8);
+                        else
+                            networkStream.Write(xByte, i * 8, 8);
+                    }
+                    networkStream.Write(xByte, 8 * umova, xByte.Length - 8 * umova);
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                networkStream.Close();
+                client.Close();
+                if (server != null)
+                    server.Stop();
+            }
+        }
+    }
+}
---
+++ Skreeen/wpfClient/MainWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace wpfClient
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        TcpClient client = new TcpClient();
+        NetworkStream network = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            client.Connect("127.0.0.1", 8888);
+
+            network = client.GetStream();
+            byte[] data = new byte[256];
+            int bytes = network.Read(data, 0, data.Length);
+
+            txBlock.Text = Encoding.UTF8.GetString(data, 0, bytes);
+
+            Task task = new Task(skreen);
+            task.Start();
+
+
+            this.Closed += closeee;
+
+        }
+        private void skreen()
+        {
+            int u = 0;
+            byte[] lng = new byte[4];
+            network.Read(lng, 0, lng.Length);
+            int leng = BitConverter.ToInt32(lng, 0);
+            byte[] da = new byte[leng];
+            while (true)
+            {
+                byte[] dat = new byte[8];
+                network.Read(dat, 0, dat.Length);
+                if (u * 8 < leng)
+                    foreach (var item in dat)
+                    {
+                        da[u] = item;
+                        u++;
+                    }
+                else
+                    for (int i = 0; i < leng - u * 8; i++)
+                    {
+                        da[u] = dat[i];
+                        u++;
+                    }
+
+
+                if (da.Length == leng)
+                {
+                    File.WriteAllBytes("test.Png", da);
+
+                    Dispatcher.Invoke(() => txBlock.Text = "All DONE!");
+                }
+            }
+        }
+        private void closeee(object sender, EventArgs e)
+        {
+            network.Close();
+            client.Close();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string response = "screen";
+            byte[] data = Encoding.UTF8.GetBytes(response);
+            network.Write(data, 0, data.Length);
+        }
+        private void Refresh_Click(object sender, RoutedEventArgs e)
+        {
+            cartoon.Stretch = Stretch.Fill;
+            BitmapImage bi3 = new BitmapImage();
+            bi3.BeginInit();
+            bi3.UriSource = new Uri("test.Png", UriKind.Relative);
+            bi3.EndInit();
+            cartoon.Source = bi3;
+            cartoon.UpdateLayout();
+        }
+    }
+}
